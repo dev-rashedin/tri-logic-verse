7196ab61f85b9bd11608703c4700b7de9efee307
dev-rashedin <rashedinislam.06@gmail.com>
2026-01-30 10:03:15 +0600

    add two number problem solved in all three language

---
+++ 4_problem_solving/01_add_two_number/add_two_number.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func addTwoNumbers(a int, b int) {
+	fmt.Println(a + b)
+}
+
+func main() {
+	addTwoNumbers(2, 3)
+}
---
+++ 4_problem_solving/01_add_two_number/add_two_number.py
@@ -0,0 +1,6 @@
+def addTwoNumber(num1, num2):
+  return num1 + num2
+
+result = addTwoNumber(2, 3)
+
+print(result)
