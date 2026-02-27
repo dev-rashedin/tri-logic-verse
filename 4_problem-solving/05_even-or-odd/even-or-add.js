ef7dfa2a63a0107eae5a68d5fca423d614e77e54
dev-rashedin <rashedinislam.06@gmail.com>
2026-02-27 10:04:08 +0600

    5th problem is solved using js

--- 4_problem-solving/05_even-or-odd/even-or-add.js
+++ 4_problem-solving/05_even-or-odd/even-or-add.js
@@ -1,14 +1,40 @@
 // 5. **Even or Odd Without `%`**  
 //    Determine whether a number is even or odd **without using the modulus operator**.
 
+
+// Using % operator
+function evenOrOddModulus(num) {
+  if (num % 2 === 0) {
+    return "It's a even number.";
+  } else {
+    return "It's a odd number.";
+  }
+}
+
+const findEvenOrOddModulus = evenOrOddModulus(4);
+console.log(findEvenOrOddModulus);
+
+// Using division and multiplication
 function evenOrOdd(num) {
-  if ((num / 2) * 2 === num) {
+  if (Math.floor(num / 2) * 2 === num) {
    return "It's a even number.";
   } else {
    return "It's a odd number.";
   }
 }
 
-
 const findEvenOrOdd = evenOrOdd(5);
 console.log(findEvenOrOdd);
+
+
+// Using bitwise AND operator
+function evenOrOddBitwise(num) {
+  if ((num & 1) === 0) {
+    return "It's a even number.";
+  } else {
+    return "It's a odd number.";
+  }
+}
+
+const findEvenOrOddBitwise = evenOrOddBitwise(6);
+console.log(findEvenOrOddBitwise);
