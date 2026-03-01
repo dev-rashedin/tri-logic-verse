b9773729eec0f70394725d5d9b4186f35010c05b
dev-rashedin <rashedinislam.06@gmail.com>
2026-03-01 11:19:43 +0600

    5th problem solved with go

--- 4_problem-solving/05_even-or-odd/even-or-add.go
+++ 4_problem-solving/05_even-or-odd/even-or-add.go
@@ -1,10 +1,12 @@
-// 5. **Even or Odd Without `%`**  
+// 5. **Even or Odd Without `%`**
 //    Determine whether a number is even or odd.
 
+package main
 
-package main;
-
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // With Modulus operator
 func evenOrOddModulus(num int) string {
@@ -15,8 +17,37 @@ func evenOrOddModulus(num int) string {
 	}
 }
 
+// With Multiplication and division
+func evenOrOdd(num float64) string {
+	if math.Floor(num / 2) * 2 == num {
+		return "It's a even number."
+	} else {
+		return "It's a odd number."
+	}
+}
+
+
+// With Bitwise AND operator
+func evenOrOddBitwise(num int) string {
+	if (num & 1) == 0 {
+		return "It's a even number."
+	} else {
+		return "It's a odd number."
+	}
+}
+
 
 func main() {
 	fmt.Println(evenOrOddModulus(4))
 	fmt.Println(evenOrOddModulus(5))
+
+	fmt.Println(evenOrOdd(4))
+	fmt.Println(evenOrOdd(5))
+
+	fmt.Println(evenOrOddBitwise(4))
+	fmt.Println(evenOrOddBitwise(5))
 }
+
+
+
+
