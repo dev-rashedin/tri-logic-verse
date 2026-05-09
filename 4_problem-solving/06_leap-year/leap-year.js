f42ca728e4749b0609302dc9e0dd3a59ba3ae876
dev-rashedin <rashedinislam.06@gmail.com>
2026-05-09 20:39:11 +0600

    leap year problem added

---
+++ 4_problem-solving/06_leap-year/leap-year.js
@@ -0,0 +1,8 @@
+// 6. **Leap Year Checker (Full Rules)**  
+//    Check if a year is a leap year (divisible by 4, not by 100 unless divisible by 400).
+
+function isLeapYear (year) {
+  if(year % 4 === 0){
+
+  } else return false
+}
